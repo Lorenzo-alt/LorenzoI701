a9a0fb55f12e9b2948683227477e2a86a1eb09c9
Lorenzo <lorenzo.juliati@terra.com.br>
2020-10-07 15:22:02 -0300

    Entrega dos arquivos, commit incial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'LorenzoI701';
+  lembretes: Array<string> = [];
+  lembrete: string;
+
+  variavelSombra: string = '10px 10px yellow';
+
+  salvar() {
+    this.lembretes = [this.lembrete, ...this.lembretes];
+    this.lembrete = '';
+  }
 }
